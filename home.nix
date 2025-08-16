0cb66e725974068f1d323ccad653320950ebe08e
Johannes Melsbach <johannes@m1-max.fritz.box>
2025-08-16 19:37:30 +0200

    fix absolute path

--- home.nix
+++ home.nix
@@ -5,6 +5,8 @@
   # Home Manager state version
   home.stateVersion = "24.05"; # Or your desired version
 
+  home.homeDirectory = "/Users/johannes";
+
   # Your user-specific packages that were here before
   home.packages = [
     pkgs.aerospace
