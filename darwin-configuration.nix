6311da8023df4bc22486c09215cb72bbbd0b4fec
Johannes Melsbach <me@melsbach.dev>
2025-08-19 09:21:47 +0200

    add pages

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -17,20 +17,19 @@
   # System-wide packages, available to all users.
   environment.systemPackages = [
     pkgs.aerospace
-    pkgs.
     pkgs.docker
     pkgs.gh
     pkgs.htop
     pkgs.neofetch
     pkgs.neovim
+    pkgs.mas
     pkgs.oh-my-zsh
     pkgs.stow
     pkgs.tmux
     pkgs.uv
     pkgs.wget
     pkgs.git
-      ];
-
+  ];
 
   # Homebrew integration is a system-level feature
   homebrew = {
@@ -61,10 +60,13 @@
       "zotero@beta"
       "font-jetbrains-mono-nerd-font"
     ];
+
     brews = [ "mas" ];
+
     masApps = {
       "Klack" = 6446206067;
       "MoneyMoney" = 872698314;
+      "Pages" = 409201541;
       "PDF Expert" = 1055273043;
       "Pixelmator Pro" = 1289583905;
       "ProtonPass for Safari" = 6502835663;
